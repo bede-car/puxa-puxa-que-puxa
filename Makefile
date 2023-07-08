36bb535f5fdaa2e08a1927e35ebcc7821ee25c41
Beatriz De carvalho branco da silva <bede-car@c1r8p1.42sp.org.br>
2023-07-08 17:05:11 -0300

    norminette

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: bede-car <bede-car@student.42sp.org.br>    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/06/08 21:48:55 by bede-car          #+#    #+#              #
-#    Updated: 2023/07/07 14:04:16 by bede-car         ###   ########.fr        #
+#    Updated: 2023/07/08 16:03:03 by bede-car         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,7 +14,7 @@ NAME				=	push_swap
 HEADER				=	./include/push_swap.h
 LIB					=	./libftprintf/libftprintf.a
 PATH_MANDATORY		=   ./source
-SOURCE				= 	parse.c ft_atol.c init.c push_swap.c small_sort.c radix_sort.c
+SOURCE				= 	parse.c ft_atol.c init.c main.c small_sort.c radix_sort.c
 CFLAG				=	-Wall -Wextra -Werror -I. -g3
 MAKEFLAGS			=	--no-print-directory
 OBJS				=	$(addprefix $(PATH_MANDATORY)/, $(SOURCE:.c=.o)) 
---
+++ source/swap.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   swap.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bede-car <bede-car@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/08 13:08:50 by bede-car          #+#    #+#             */
+/*   Updated: 2023/07/08 15:56:14 by bede-car         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/push_swap.h"
+
+//sa (swap a): Swap the first 2 elements at the top of stack a.
+//Do nothing if there is only one or no elements.
+t_data swap_a(t_data *stack_a, t_data *stack_a)
+{
+	if ()
+
+	ft_printf("sa\n");
+}
+
+t_data swap_b(t_data *stack_a, t_data *stack_a)
+{
+	if ()
+
+	ft_printf("sa\n");
+}
+
+t_data swap_bouth(t_data *stack_a, t_data *stack_a)
+{
+	if ()
+
+	ft_printf("sa\n");
+}
